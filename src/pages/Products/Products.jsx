e08a22c79bf027ded5c6e672b049ae7ac8686e90
mikalojuszrg <mikalojus.zirgulis@gmail.com>
2023-01-31 22:01:49 +0200

    select added

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { ProductContext } from "../../contexts/ProductContext";
 import styled from "styled-components";
 import { capitalizeFirstLetter } from "../../utils/string";
+import Select from "react-select";
 
 const Products = () => {
   const { category } = useParams();
@@ -13,21 +14,40 @@ const Products = () => {
   );
 
   return (
-    <ProductsContainer>
-      {categoryProducts.map((product) => (
-        <ProductItem key={product.id}>
-          <img src={product.picUrl[0]} alt="product" />
+    <div>
+      <FiltersContainer>
+        <Filter>
+          <Select isMulti name="colors" options={[]} />
+        </Filter>
+      </FiltersContainer>
+      <ProductsContainer>
+        {categoryProducts.map((product) => (
+          <ProductItem key={product.id}>
+            <img src={product.picUrl[0]} alt="product" />
 
-          <ProductName>
-            {capitalizeFirstLetter(product.name.toLowerCase())}
-          </ProductName>
-        </ProductItem>
-      ))}
-    </ProductsContainer>
+            <ProductProperty>
+              {capitalizeFirstLetter(product.name.toLowerCase())}
+            </ProductProperty>
+            <ProductProperty>€{product.price}</ProductProperty>
+          </ProductItem>
+        ))}
+      </ProductsContainer>
+    </div>
   );
 };
 
 export default Products;
+
+const FiltersContainer = styled.div`
+  padding-left: 40px;
+  padding-top: 40px;
+  padding-right: 60px;
+  /* display: flex; */
+`;
+
+const Filter = styled.div`
+  width: 250px;
+`;
 
 const ProductsContainer = styled.div`
   display: grid;
@@ -37,20 +57,25 @@ const ProductsContainer = styled.div`
 `;
 
 const ProductItem = styled.div`
-  margin: 24px;
+  margin-right: 24px;
+  margin-bottom: 24px;
   background-color: #ffffff;
   display: flex;
   flex-direction: column;
+  border-radius: 5px;
+  border: 1px solid #e7e3e1;
 
   img {
     flex: 1;
-    border-radius: 5px;
     width: 100%;
     height: 100%;
     object-fit: cover;
+    border-radius: 5px;
   }
 `;
 
-const ProductName = styled.p`
-  /* margin: 0; */
+const ProductProperty = styled.p`
+  margin-top: 0;
+  margin-bottom: 8px;
+  margin-left: 16px;
 `;
