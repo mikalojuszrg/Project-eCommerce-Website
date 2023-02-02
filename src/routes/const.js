f5a1636b488f991d7b05af948bdbd71b2f609c9d
mikalojuszrg <mikalojus.zirgulis@gmail.com>
2023-02-02 20:02:56 +0200

    new route

--- src/routes/const.js
+++ src/routes/const.js
@@ -1,9 +1,12 @@
 import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home/Home";
+import Product from "../pages/Product/Product";
 import Products from "../pages/Products/Products";
 
 export const HOME_PATH = "/";
 export const PRODUCT_LIST_PATH = `${HOME_PATH}:category`;
+export const PRODUCT_PATH = `${PRODUCT_LIST_PATH}/:productId`;
+// export const PRODUCT_PATH = `/:category/:id`;
 
 export const mainLayoutRoutes = {
   Layout: MainLayout,
@@ -15,6 +18,10 @@ export const mainLayoutRoutes = {
     {
       path: PRODUCT_LIST_PATH,
       Component: Products,
+    },
+    {
+      path: PRODUCT_PATH,
+      Component: Product,
     },
   ],
 };
