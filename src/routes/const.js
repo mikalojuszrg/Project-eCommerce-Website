31bc5e8121fce26209d7df1a632236b807619be5
mikalojuszrg <mikalojus.zirgulis@gmail.com>
2023-02-07 20:26:56 +0200

    added login and cart routes

--- src/routes/const.js
+++ src/routes/const.js
@@ -1,12 +1,15 @@
 import MainLayout from "../layouts/MainLayout";
+import Cart from "../pages/Cart/Cart";
 import Home from "../pages/Home/Home";
 import Product from "../pages/Product/Product";
 import Products from "../pages/Products/Products";
+import Login from "../pages/Login/Login";
 
 export const HOME_PATH = "/";
-export const PRODUCT_LIST_PATH = `${HOME_PATH}:category`;
+export const PRODUCT_LIST_PATH = `${HOME_PATH}category/:category`;
 export const PRODUCT_PATH = `${PRODUCT_LIST_PATH}/:productId`;
-// export const PRODUCT_PATH = `/:category/:id`;
+export const LOGIN = "/login";
+export const CART = "/cart";
 
 export const mainLayoutRoutes = {
   Layout: MainLayout,
@@ -23,5 +26,8 @@ export const mainLayoutRoutes = {
       path: PRODUCT_PATH,
       Component: Product,
     },
+    { path: CART, Component: Cart },
+
+    { path: LOGIN, Component: Login },
   ],
 };
